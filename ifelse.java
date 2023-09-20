54934e5bc2fed154ab07e0cab0393f236219199a
Muthuraj222 <144137115+Muthuraj222@users.noreply.github.com>
2023-09-20 20:43:06 +0530

    Add files via upload

---
+++ ifelse.java
@@ -0,0 +1,14 @@
+class Ifelse{
+    public static void main(String args[])
+    {
+        int x=1;
+        int y=1;
+        if(x!=y)
+        {//single statement we dont use curly braces
+            System.out.println("Helloworld");
+        }
+        else{//more than one statement we use curly braces
+            System.out.println("no hello");
+        }
+    }
+}
